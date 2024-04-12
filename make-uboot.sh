83c3ed509f022eed5e4ac0aaa5ad609c24103512
张世争 <zhangsz0516@163.com>
2024-04-13 07:45:16 +0800

    backup 2024-04-13

--- make-uboot.sh
+++ make-uboot.sh
@@ -6,5 +6,6 @@
 #make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- $1 $2 $3 $4
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mrproper
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6sabresd_defconfig
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6sabresd_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6q_topeet_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
